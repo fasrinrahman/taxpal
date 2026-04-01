656918bcb787284b7ff1cf7c596a6b105741b72e
fasrinrahman <mfasrinrahman@gmail.com>
2026-04-01 22:56:01 +0530

    ADDED: Simplify Section Image CHanging for Each Card Section

--- script.js
+++ script.js
@@ -56,4 +56,44 @@ feature_tabs.forEach((feature_tab, index) => {
     // change screan shot by clicking to reaalted image  with its index
     feature_images.src = images[index];
   });
+
+  // Simplify Tabs for TaxPal Prject
+  // select tabs , images
+  const simplify_cards = document.querySelectorAll(".simplify_card");
+
+  const simplify_images = document.querySelector("#simplify_img");
+
+  const simplify_images_arr = [
+    // img1, img2 .. with tab order in notion
+    "https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpayroll.948a58d4.png&w=3840&q=75",
+    "https://salient.tailwindui.com/_next/image?url=%2F_next%2F/static%2Fmedia%2Fexpenses.6dca4b99.png&w=3840&q=75",
+    "https://salient.tailwindui.com/_next/image?url=%2F_next%2F/static%2Fmedia%2Fvat-returns.90e74b9d.png&w=3840&q=75",
+  ];
+
+  // queryselectorAll return nodellist - we can use for loop [tabs]
+  simplify_cards.forEach((simplify_card, index) => {
+    simplify_card.addEventListener("click", () => {
+      // remove the active styles from all other tabs
+      simplify_cards.forEach((card_ele) => {
+        card_ele.classList.remove(
+          "active",
+          "bg-white/10",
+          "ring-1",
+          "ring-white/10",
+          "ring-inset",
+        );
+        // assign the active classes to the clicked tab
+        simplify_card.classList.add(
+          "active",
+          "bg-white/10",
+          "ring-1",
+          "ring-white/10",
+          "ring-inset",
+        );
+      });
+
+      // change screan shot by clicking to reaalted image  with its index
+      simplify_images.src = simplify_images_arr[index];
+    });
+  });
 });
